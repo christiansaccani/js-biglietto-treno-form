a75ac7d13d8f0f4bfd1980e45307d80b7b0c72c6
christiansaccani <christian.saccani99@gmail.com>
2024-01-31 00:54:00 +0100

    Exercise Done!

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let passenger;
 let kmtot;
 let price;
 let age;
+let cab;
+let cp;
 const genButton = document.querySelector("#generatebutton");
+const clButton = document.querySelector("#clearbutton");
 
 genButton.addEventListener("click", 
-    function() {
+    function(event) {
+        event.preventDefault();   //To block auto-refresh
+
         console.log("Invio Click")
 
         passenger = document.getElementById("passengername").value;
@@ -23,21 +28,50 @@ genButton.addEventListener("click",
             if (typeof age == 'number' && !Number.isNaN(age) && age > 0) {
                 console.log('✅ age value is a number'); 
 
+                //Random Cab Number
+
+                function getRandomInt(min, max) {
+                    return Math.floor(Math.random() * (max - min + 1) + min);
+                }
+                cab = getRandomInt(1, 12);
+
+                // Random CP Number
+
+                cp = Math.floor(Math.random()*90000) + 10000;
+
                 if (age < 18) {
                     console.log('Il passeggero è minorenne, si applica sconto del 20%!');
                     price = (price * 80 / 100).toFixed(2);     //arrotondare il risultato ai decimali
                     console.log("Prezzo finale: €", price);
 
+                    document.getElementById('name_from_input').innerHTML = passenger;
+                    document.getElementById('coupon').innerHTML = "-20%";
+                    document.getElementById('random_cab').innerHTML = cab;
+                    document.getElementById('random_cp').innerHTML = cp;
+                    document.getElementById('ticket_price').innerHTML = `€ ${price}`;
+
                 } else if (age >= 65) {
                     console.log('Il passeggero è Over65, si applica sconto!');
                     price = (price * 60 / 100).toFixed(2);
                     console.log("Prezzo finale: €", price);
+
+                    document.getElementById('name_from_input').innerHTML = passenger;
+                    document.getElementById('coupon').innerHTML = "-40%";
+                    document.getElementById('random_cab').innerHTML = cab;
+                    document.getElementById('random_cp').innerHTML = cp;
+                    document.getElementById('ticket_price').innerHTML = `€ ${price}`;
               
                 } else {
                     console.log('Il passeggero non ha diritto ad alcuno sconto.');
                     console.log("Prezzo finale: €", price);
 
+                    document.getElementById('name_from_input').innerHTML = passenger;
+                    document.getElementById('coupon').innerHTML = "Nessuno";
+                    document.getElementById('random_cab').innerHTML = cab;
+                    document.getElementById('random_cp').innerHTML = cp;
+                    document.getElementById('ticket_price').innerHTML = `€ ${price}`;
                 }
+                
 
             } else {
                 console.log('⛔️ value is NOT a number');
@@ -50,5 +84,18 @@ genButton.addEventListener("click",
         console.log('⛔️ value is NOT a number');
         alert("Valore Km inserito non valido, la preghiamo di riprovare!");        
         }
+    }
+    
+)
+
+
+
+// Clear Fields Button
+
+clButton.addEventListener("click", 
+    function() {
+        document.getElementById("passengername").value = "";
+        document.getElementById("kmtxt").value = "";
+        document.getElementById("agetxt").value = "";
     }
 )
